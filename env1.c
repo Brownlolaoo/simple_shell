527e68382560c10e08c19eafe3b34628a5e5cba3
mohzaey <moh.balogun@gmail.com>
2022-06-08 00:22:03 -0700

    by omotolani damilola

--- env1.c
+++ env1.c
@@ -1,5 +1,5 @@
 #include "main.h"
- 
+
 /**
  * cmp_env_name - compares env variables names
  * with the name passed.
@@ -10,19 +10,19 @@
  */
 int cmp_env_name(const char *nenv, const char *name)
 {
-            int i;
- 
-            for (i = 0; nenv[i] != '='; i++)
-            {
-                        if (nenv[i] != name[i])
-                        {
-                                    return (0);
-                        }
-            }
- 
-            return (i + 1);
+	int i;
+
+	for (i = 0; nenv[i] != '='; i++)
+	{
+		if (nenv[i] != name[i])
+		{
+			return (0);
+		}
+	}
+
+	return (i + 1);
 }
- 
+
 /**
  * _getenv - get an environment variable
  * @name: name of the environment variable
@@ -33,28 +33,28 @@ int cmp_env_name(const char *nenv, const char *name)
  */
 char *_getenv(const char *name, char **_environ)
 {
-            char *ptr_env;
-            int i, mov;
- 
-            /* Initialize ptr_env value */
-            ptr_env = NULL;
-            mov = 0;
-            /* Compare all environment variables */
-            /* environ is declared in the header file */
-            for (i = 0; _environ[i]; i++)
-            {
-                        /* If name and env are equal */
-                        mov = cmp_env_name(_environ[i], name);
-                        if (mov)
-                        {
-                                    ptr_env = _environ[i];
-                                    break;
-                        }
-            }
- 
-            return (ptr_env + mov);
+	char *ptr_env;
+	int i, mov;
+
+	/* Initialize ptr_env value */
+	ptr_env = NULL;
+	mov = 0;
+	/* Compare all environment variables */
+	/* environ is declared in the header file */
+	for (i = 0; _environ[i]; i++)
+	{
+		/* If name and env are equal */
+		mov = cmp_env_name(_environ[i], name);
+		if (mov)
+		{
+			ptr_env = _environ[i];
+			break;
+		}
+	}
+
+	return (ptr_env + mov);
 }
- 
+
 /**
  * _env - prints the evironment variables
  *
@@ -63,18 +63,18 @@ char *_getenv(const char *name, char **_environ)
  */
 int _env(data_shell *datash)
 {
-            int i, j;
- 
-            for (i = 0; datash->_environ[i]; i++)
-            {
- 
-                        for (j = 0; datash->_environ[i][j]; j++)
-                                    ;
- 
-                        write(STDOUT_FILENO, datash->_environ[i], j);
-                        write(STDOUT_FILENO, "\n", 1);
-            }
-            datash->status = 0;
- 
-            return (1);
+	int i, j;
+
+	for (i = 0; datash->_environ[i]; i++)
+	{
+
+		for (j = 0; datash->_environ[i][j]; j++)
+			;
+
+		write(STDOUT_FILENO, datash->_environ[i], j);
+		write(STDOUT_FILENO, "\n", 1);
+	}
+	datash->status = 0;
+
+	return (1);
 }
